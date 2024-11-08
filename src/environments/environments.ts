f828c48143e3120400db2741866d9cc316c7e2a6
sunilhopt <sunil.verma@hopt.ai>
2024-11-08 20:33:09 +0530

    added frontend

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  apiUrl: "https://genai-for-marketing-backend-apis-124965173834.us-central1.run.app/marketing-api",
+  dialogflowCxAgentId:"",
+  firebaseConfig : {
+    apiKey: "AIzaSyB-Eh7qzwVK5yPwyBMg3a0Th07cDX7pHgw",
+    authDomain: "storied-glazing-440613-v8.firebaseapp.com",
+    projectId: "storied-glazing-440613-v8",
+    storageBucket: "storied-glazing-440613-v8.firebasestorage.app",
+    messagingSenderId: "124965173834",
+    appId: "1:124965173834:web:1e0e17bdfbda181d62c4b3"
+  }
+}
